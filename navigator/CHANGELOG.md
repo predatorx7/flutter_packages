4193ba2cf90204ccd44b1c25264fcc1490babdf1
predatorx7 <smushaheed@outlook.com>
2021-12-09 18:30:58 +0530

    Add missing exports

--- navigator/CHANGELOG.md
+++ navigator/CHANGELOG.md
@@ -1,3 +1,7 @@
+## 0.2.1+1
+
+* Add missing exports
+
 ## 0.2.1
 
 * Add navigateFromString in LinkRouter
--- navigator/lib/navigator.dart
+++ navigator/lib/navigator.dart
@@ -1,6 +1,9 @@
 library navigator;
 
 export 'src/configurations.dart';
+export 'src/error.dart';
+export 'src/link_navigation.dart';
 export 'src/logger.dart';
+export 'src/page_route.dart';
 export 'src/path.dart';
 export 'src/typedefs.dart';
