da3303377fa39bb9431aab49e25ea44a67a4a506
= <mushaheed.syed@enpointe.io>
2022-05-20 17:15:57 +0530

    Mark html.dart as deprecated

--- magnific_core/lib/strings/html.dart
+++ magnific_core/lib/strings/html.dart
@@ -1,4 +1,7 @@
+// ignore_for_file: provide_deprecation_message
+
 /// Wraps a string with div with id `wrapWithDiv`.
+@deprecated
 String wrapWithDiv(String? html) {
   return '<div id="wrapWithDiv">${html ?? ""}</div>';
 }
